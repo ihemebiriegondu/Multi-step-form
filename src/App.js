8bf3db01befc9b51715256a1d9c58e1990b89e5f
ihemebiriegondu <ihemebiriegondu@gmail.com>
2023-08-07 07:43:58 +0100

    updated the responsiveness

--- src/App.js
+++ src/App.js
@@ -8,11 +8,11 @@ import Summary from "./components/mainforms/summary";
 function App() {
   sessionStorage.setItem("plans", JSON.stringify({}));
   sessionStorage.setItem("addOns", JSON.stringify([]));
-  
+
   return (
     <Router>
-      <main className="main flex absolute top-0 bottom-0 w-full p-4 lg:p-8 md:pb-4 lg:pb-8">
-        <nav className="bg-sidebarMobile md:bg-sidebarDesktop pt-2 sm:pt-10 pb-32 md:pb-0 md:pt-0 absolute top-0 left-0 right-0 md:static md:basis-1/3 xlg:basis-1/4 2xl:basis-1/5 bg-no-repeat bg-cover md:rounded-xl">
+      <main className="main bg-Magnolia relative md:bg-white h-full flex w-full p-4 lg:p-8 mb-6 overflow-x-hidden">
+        <nav className="md:h-full bg-sidebarMobile md:bg-sidebarDesktop pt-2 sm:pt-10 pb-32 md:pb-0 md:pt-0 absolute md:static top-0 left-0 right-0 md:static md:basis-1/3 xlg:basis-1/4 2xl:basis-1/5 bg-no-repeat bg-cover md:rounded-3xl">
           <NavBar />
         </nav>
         <section className="px-4 xsm:px-6 sm:px-10 lg:px-16 xlg:px-20 py-10 bg-white absolute top-28 sm:top-48 left-2 xsm:left-4 sm:left-10 right-2 xsm:right-4 sm:right-10 shadow md:shadow-none md:static rounded-xl md:rounded-none md:basis-2/3 xlg:basis-3/4 2xl:basis-4/5">
